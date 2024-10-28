778ecf4e0976e0e06ff59f2533f1fe7542ecdcc9
ayaanalam07 <ayaanabaf@gmail.com>
2024-10-28 14:06:33 +0500

    website

--- 1st-page/app.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const mobileNav = document.getElementById("mobile-menu");
-    if (mobileNav.style.width === "0px" || mobileNav.style.width === "") {
-        mobileNav.style.width = "100%"; 
-    } else {
-        mobileNav.style.width = "0"; 
-    }
-}
-
